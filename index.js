8516e6acc5108f39d7f5de55be7a739e31d5cece
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-01-10 11:53:15 +0300

    cocmplete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((drivers) => drivers.toLowerCase() === name.toLowerCase());
+}
+findMatching(["Alice", "Bob", "Charlie"], "alice");
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) => driver.startsWith(letters));
+  }
+  fuzzyMatch(["Alice", "Bob", "Charlie"], "Al"); // Output: ["Alice"]
+  function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+  }
+  const drivers = [
+    { name: "Alice", hometown: "New York" },
+    { name: "Bob", hometown: "Los Angeles" },
+  ];
+  matchName(drivers, "Alice"); 
+  
+  
